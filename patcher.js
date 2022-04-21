88adf06ece05641a095f3b857fb25dffbb673cac
Alluseri <42897348+Alluseri@users.noreply.github.com>
2022-04-21 20:16:42 +0500

    Add files via upload

---
+++ patcher.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+
+var __PATCHES = null;
+
+console.log("/json patcher by Nekoseri/");
+console.log("Reading patches...");
+__PATCHES = JSON.parse(fs.readFileSync("__PATCHES.json", "utf8").toString());
+console.log("Working...");
+
+function setByChain(content, chain, set) {
+	var byref = content;
+	while (chain.length > 1) {
+		byref = byref[chain.shift()];
+	}
+	byref[chain.shift()] = set;
+	return content;
+}
+function getByChain(content, chain) {
+	var byref = content;
+	while (chain.length > 1) {
+		byref = byref[chain.shift()];
+	}
+	return byref[chain.shift()];
+}
+function fi(og, res) {
+	if (typeof(res) == "string") {
+		var sx = res.split(":");
+		return og.find((x)=>x[sx[0]]==sx[1]);
+	} else return og[res < 0 ? (og.length + res) : res];
+}
+function runRecursivePatches(content, patch, chain) {
+	for (var xk in patch) {
+		let chcl = chain.map((x)=>x);
+		chcl.push(xk);
+		if (patch[xk].constructor.name == "Object") {
+			runRecursivePatches(content, patch[xk], chcl);
+		} else if (patch[xk].constructor.name == "Array") {
+			var resx = patch[xk];
+			if (resx[0] == "set") {
+				setByChain(content, chcl, resx[1]);
+				console.log("Applied singular 'set' patch to "+xk);
+			} else {
+				var og = getByChain(content, chcl);
+				for (var res_ in resx) {
+					var res = resx[res_];
+					switch (res[0]) {
+					case "injectPush":
+						var sym = fi(og, res[1]);
+						var rt = res[2];
+						for (var pk in rt) {
+							var rtx = rt[pk];
+							for (var pkx in rtx) {
+								sym[pk].push(rtx[pkx]);
+							}
+						}
+						break;
+					case "delete":
+						delete (fi(og, res[1])[res[2]]);
+						break;
+					case "push":
+						for (var x__ in res[1]) {
+							og.push(res[1][x__]);
+						}
+						break;
+					case "set":
+						var sym = fi(og, res[1]);
+						var rt = res[2];
+						for (var pk in rt) {
+							sym[pk] = rt[pk];
+						}
+						break;
+					}
+					console.log("Applied '"+res[0]+"' patch to "+xk);
+				}
+			}
+		}
+	}
+}
+
+for (var file in __PATCHES) {
+	var patches = __PATCHES[file];
+	if (!file.endsWith(".json")) file = file + ".json";
+	var fileJsonContent = JSON.parse(fs.readFileSync(file, "utf8").toString());
+	console.log("Imported "+file+" successfully.");
+	var backupFileName = "UNPATCHED_"+file;
+	fs.writeFileSync(backupFileName, JSON.stringify(fileJsonContent, null, "\t"));
+	console.log("Wrote a backup to "+backupFileName+".");
+	runRecursivePatches(fileJsonContent, patches, []);
+	console.log("Patched "+file+" successfully, saving...");
+	fs.writeFileSync(file, JSON.stringify(fileJsonContent, null, "\t"));
+	console.log("Saved "+file+" successfully!");
+}
